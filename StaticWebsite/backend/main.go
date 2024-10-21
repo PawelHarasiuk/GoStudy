a71c136c00a77ae66234300aa41554ff85b8c330
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-21 04:40:30 +0200

    created backend with lambda and api gateway

--- StaticWebsite/backend/main.go
+++ StaticWebsite/backend/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+)
 
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Hello from lambda")
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		//Headers:           nil,
+		Body: "Response body",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
